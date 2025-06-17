b3df354e6ab48833314bad55ddd1175479c823c8
Fedor <proterrarea@gmail.com>
2025-06-17 09:13:03 +0300

    Added model choice when program can't load model from config

--- config.py
+++ config.py
@@ -1,4 +1,27 @@
+import io
 import json
+import os
+
+
+DEFAULT_CONFIG = {
+    "model": {
+        "filename": "none.gguf"
+    }
+}
+
+
+def create_config(func):
+    def wrapper(*args, **kwargs):
+        try:
+            with open("config.json", "r") as file:
+                config = json.load(file)
+                return func(*args, **kwargs)
+        except FileNotFoundError:
+            with open("config.json", "w") as file:
+                file.write(json.dumps(DEFAULT_CONFIG, indent=2))
+                return func(*args, **kwargs)
+
+    return wrapper
 
 
 def get_prompt():
@@ -8,14 +31,42 @@ def get_prompt():
 
 
 def format_chat(history, user_input):
-    chat = "<|system|>\n" + get_prompt().strip() + "\n"
-    for u, a in history:
+    prompt = get_prompt().strip()
+    chat = "<|system|>\n" + prompt + "\n"
+
+    for i, (u, a) in enumerate(history):
+        if i > 0 and i % 5 == 0:
+            chat += "<|system|>\n" + prompt + "\n"
         chat += f"<|user|>\n{u}\n<|assistant|>\n{a}\n"
-    chat += f"<|user|>\n{user_input}\n<|assistant|>\n"
+
+    chat += "<|user|>\n" + user_input + "\n<|assistant|>\n"
     return chat
 
 
+@create_config
 def get_model_path():
     with open('config.json', 'r', encoding='utf-8') as file:
         config = json.load(file)
     return f"models/{config['model']['filename']}"
+
+
+@create_config
+def find_models():
+    models = []
+    for file in os.listdir("models"):
+        if file.endswith(".gguf"):
+            models.append(file)
+    return models
+
+
+@create_config
+def write_models(model):
+    print(model)
+    with open("config.json", "r") as file:
+        config = json.load(file)
+
+    config["model"]["filename"] = model
+
+    with open("config.json", "w") as file:
+        json.dump(config, file, indent=2)
+
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from llama_cpp import Llama
-from config import get_prompt, format_chat, get_model_path
+from config import get_prompt, format_chat, get_model_path, find_models, write_models
 from history import write_history, read_history, print_history, delete_history
 
 print(Llama.__doc__)
 
 def main():
-    try:
-        llm = Llama(
-            model_path=get_model_path(),
-            n_ctx=16384,
-            n_threads=8,
-            n_gpu_layers=-1,
-            chat_format="chatml",
-            verbose=False,
-        )
-    except ValueError:
-        print(f"Модель по пути {get_model_path()} не найдена\n"
-              f"Для использования модели поместите её в папку models\n"
-              f"После добавьте полное название файла в config.json")
-        return
+    while True:
+        try:
+            llm = Llama(
+                model_path=get_model_path(),
+                n_ctx=32768,
+                n_threads=8,
+                chat_format="chatml",
+                verbose=False,
+            )
+            break
+        except ValueError:
+            print(f"Модель по пути {get_model_path()} не найдена\n"
+                  f"Для использования модели поместите её в папку models\n"
+                  f"После добавьте полное название файла в config.json\n\n"
+                  f"Или выберете из списка моделей:")
+            models = find_models()
+
+            for index, model in enumerate(models):
+                print(f"{index}) {model}")
+
+            write_models(models[int(input("Введите номер модели: "))])
 
     print("Модель загружена. ‘exit’ для выхода, clear для очистки истории.")
     history = read_history()
@@ -39,7 +46,7 @@ def main():
         stream = llm(
             prompt,
             max_tokens=200,
-            temperature=0.6,
+            temperature=0.8,
             top_p=0.9,
             repeat_penalty=1.2,
             stream=True,
